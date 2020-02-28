1033c4a72b9b30e87b9621259669dd898a183bd1
s.zomerfeld <sebastian.zomerfeld@gmx.net>
2020-02-28 14:50:35 +0100

    Calorie Page Logik

--- BlazorFitnessApp/Excercise.cs
+++ BlazorFitnessApp/Excercise.cs
@@ -7,7 +7,31 @@ namespace BlazorFitnessApp
 {
     public class Excercise
     {
-        public double PAL { get; set; }
-        public double Time { get; set; }
+        private double _pal;
+        private double _time;
+
+        public double PAL
+        {
+            get
+            {
+                return _pal;
+            }
+            set
+            {
+                _pal = value;
+            }
+        }
+        public double Time
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                _time = value;
+            }
+        }
+
     }
 }
