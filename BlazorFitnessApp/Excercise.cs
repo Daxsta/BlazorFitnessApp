f25ddfb2fa6adeab450d761be3a1d95e7a9ee841
s.zomerfeld <sebastian.zomerfeld@gmx.net>
2020-02-28 12:54:56 +0100

    first commit

---
+++ BlazorFitnessApp/Excercise.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFitnessApp
+{
+    public class Excercise
+    {
+        public double PAL { get; set; }
+        public double Time { get; set; }
+    }
+}
